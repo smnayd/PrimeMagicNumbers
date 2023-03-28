d134ff44dc69947c1263402ddc3e12fa16bb3a12
Semanur Aydınlık <sm.ayd02@gmail.com>
2023-03-29 02:25:08 +0300

    Add project files.

---
+++ RandomMagic/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace RandomMagic
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(428, 77);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.button1.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Location = new System.Drawing.Point(754, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(166, 69);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Select";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(428, 188);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(185, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.button2.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(754, 166);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 67);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Select";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.listBox1.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 26;
+            this.listBox1.Location = new System.Drawing.Point(410, 253);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(270, 368);
+            this.listBox1.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Navy;
+            this.label1.Location = new System.Drawing.Point(73, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(334, 26);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Enter number to be find magical:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MV Boli", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Navy;
+            this.label2.Location = new System.Drawing.Point(73, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(321, 26);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Enter number to be find prime:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(1206, 655);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ RandomMagic/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace RandomMagic
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public bool IsPrime(int number)
+        {
+            for (int i = 2; i <= number/2; i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public bool IsMagicNumber(int number)
+        {
+            int sum = 0;
+            int temp = number;
+            while (temp > 0)
+            {
+                int digit = temp % 10;
+                sum += Factorial(digit);
+                temp /= 10;
+            }
+            return sum == number;
+        }
+        public int Factorial(int number)
+        {
+            int fact = 1;
+            while(number > 0)
+            {
+                fact *= number;
+                number--;
+            }
+            return fact;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num;
+            if (int.TryParse(textBox1.Text, out num))
+            {
+                Thread t1 = new Thread(() =>
+                {
+                    List<int> magicNumbers = new List<int>();
+                    for (int i = 1; i <= num; i++)
+                    {
+                        if (IsMagicNumber(i))
+                        {
+                            magicNumbers.Add(i);
+                        }
+                    }
+                    listBox1.Invoke(new Action(() =>
+                    {
+                        listBox1.Items.Clear();
+                        foreach (int magicNumber in magicNumbers)
+                        {
+                            listBox1.Items.Add(magicNumber);
+                        }
+                    }));
+                });
+                t1.Start();
+            }
+            else
+            {
+                MessageBox.Show("Please enter correct number!");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int num;
+            if (int.TryParse(textBox2.Text, out num))
+            {
+                Thread t2 = new Thread(() =>
+                {
+                    List<int> primeNumbers = new List<int>();
+                    for (int i = 2; i <= num; i++)
+                    {
+                        if (IsPrime(i))
+                        {
+                            primeNumbers.Add(i);
+                        }
+                    }
+
+                    listBox1.Invoke(new Action(() =>
+                    {
+                        listBox1.Items.Clear();
+                        foreach (int primeNumber in primeNumbers)
+                        {
+                            listBox1.Items.Add(primeNumber);
+                        }
+                    }));
+                });
+                t2.Start();
+            }
+            else
+            {
+                MessageBox.Show("Please enter correct number!");
+            }
+
+        }
+
+        
+    }
+}
